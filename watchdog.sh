cea17af2d4cc39dc2dddbf8bd6c70f7ee4ff0d4e
Yi Qiao <atrusqiao@gmail.com>
2015-06-03 10:52:13 -0600

    Updated watchdog.sh

--- watchdog.sh
+++ watchdog.sh
@@ -2,11 +2,15 @@
 visited=0
 while true; do
 	sleep 60
+	timestamp=$(date +%T)
 	netstat -t | grep ':8004'
 	ret=$?
+	echo "[${timestamp}] visited=${visited}, ret=${ret}"
 	if [ $ret != 0 -a $visited -eq 1 ]; then
+		echo "[${timestamp}] network connection terminated. shutting down..."
 		kill $(cat /var/run/supervisord.pid)
 	elif [ $ret == 0 -a $visited -eq 0 ]; then
+		echo "[${timestamp}] network connection established. setting visited to 1"
 		visited=1
 	fi
 done
